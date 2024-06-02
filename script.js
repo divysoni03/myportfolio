3d1fdf34660d20457e245b8225f3ea26099e676c
divy soni <chetanshudha267@gmail.com>
2024-06-02 11:58:00 +0530

    done hamburger nav bar

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
+    const menu = document.querySelector('.menu-links');
+    const icon = document.querySelector('.hamburger-icon');
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const hamMenu = document.querySelector('.ham-menu');
+const offscreenMenu = document.querySelector('.off-screen-menu');
+hamMenu.addEventListener('click',() =>{
+    hamMenu.classList.toggle('active');
+    offscreenMenu.classList.toggle('active');
+})
