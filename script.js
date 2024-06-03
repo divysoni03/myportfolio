0719b464cd81c0fdc0acb65a2551304b6640a66b
divy soni <chetanshudha267@gmail.com>
2024-06-03 17:10:46 +0530

    new features added

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu(){
     hamMenu.classList.toggle("active");
     offscreenMenu.classList.toggle("active");
 }
+
+// let sections = document.querySelector('section');
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop -150 ;
+//         let height = sec.offsetHeight;
+
+//         if(top >= offset && top < offset + height){
+//             sec.classList.add('show-animate');
+//         }
+//         else {
+//             sec.classList.remove('show-animate');
+//         }
+//     })
+// }
