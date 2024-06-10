2f1b005d678dee250f8fbc9f4214eae6cc6e1911
divy soni <chetanshudha267@gmail.com>
2024-06-10 15:23:07 +0530

    onclick bug solved

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ function toggleMenu(){
     offscreenMenu.classList.toggle("active");
 }
 
+// function toggleBoxShadow(event) {
+//     const element = event.target;
+//     element.classList.add('box-shadow-change');
+
+//     // Remove the class after a specified duration (e.g., 1 second)
+//     setTimeout(() => {
+//         element.classList.remove('box-shadow-change');
+//     }, 1000); // 1000 milliseconds = 1 second
+// }
+
+// Add click event listener to the element
+document.querySelector('.element').addEventListener('click', toggleBoxShadow);
 // let sections = document.querySelector('section');
 // window.onscroll = () => {
 //     sections.forEach(sec => {
